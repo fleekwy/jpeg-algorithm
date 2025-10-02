30d2aed6506733df60ef69c792a5659c0238cce5
Daniil Kondratyuk <daniilkondratuk@mail.com>
2025-10-02 20:51:02 +0300

    add logger; del test1; setup format "compress"; add centring before dct (block-128)

--- jpeg_compressor.py
+++ jpeg_compressor.py
@@ -1,5 +1,7 @@
+import sys
 import numpy as np
 from PIL import Image
+import logging
 
 class JpegCompressor:
     """Класс JPEG-компрессора.
@@ -14,10 +16,18 @@ class JpegCompressor:
     
 # private 
     
-    def __init__(self, image_path=None):
+    def __init__(self):
         """Первоначальная инициализация состояния объекта"""
         
-        print("\n__init__:  <start>")
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
+        if not self.logger.handlers:
+            handler = logging.StreamHandler(sys.stdout)
+            formatter = logging.Formatter('%(levelname)s: %(message)s')
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+        
+        self.logger.info("__init__:  <start>")
         
         self._original_pixels = None
         self._compressed_pixels = None
@@ -42,14 +52,22 @@ class JpegCompressor:
             [99, 99, 99, 99, 99, 99, 99, 99],
             [99, 99, 99, 99, 99, 99, 99, 99],
             [99, 99, 99, 99, 99, 99, 99, 99]], dtype=np.uint8)
-        
-        if image_path:
-            self.load_image(image_path)
             
-        print("__init__:  <end>\n")
+        self.logger.info("__init__:  <end>")
         
         
 # protected
+
+    def _reset_state(self):
+        
+        self.logger.info("_reset_state:  <start>")
+        
+        self._original_pixels = None
+        self._compressed_pixels = None
+        self.quality = None
+        
+        self.logger.info("_reset_state:  <end>")
+
 
     def _create_dctII_matrix(self, N):
         dct_mat = np.zeros((N, N))
@@ -66,7 +84,7 @@ class JpegCompressor:
         else:
             scale = 200 - 2 * quality
             
-        #print(f"{scale=}")
+        #self.logger.info(f"{scale=}")
 
         # ВАЖНО: привести к float32, чтобы избежать переполнения
         table = table.astype(np.float32)
@@ -75,11 +93,10 @@ class JpegCompressor:
         return np.clip(scaled, 1, 255).astype(np.uint8)
 
 
-
     def _rgb_to_ycbcr(self, rgb_pixels):
         """Конвертация RGB в YCbCr"""
         
-        print("\n_rgb_to_ycbcr:  <start>")
+        self.logger.info("_rgb_to_ycbcr:  <start>")
         
         
         # Создаем пустой массив для YCbCr
@@ -112,14 +129,14 @@ class JpegCompressor:
         # pixels_uint8 = np.clip(self._pixels, 0, 255).astype(np.uint8)
         # Image.fromarray(pixels_uint8).save("data/output_1.jpeg")
         
-        print("_rgb_to_ycbcr:  <end>\n")
+        self.logger.info("_rgb_to_ycbcr:  <end>")
         return ycbcr_pixels
         
     
     def _chroma_subsampling(self, ycbcr_pixels):
         """Хроматическое прореживание 4:2:0 с паддингом и усреднением"""
     
-        print("\n_chroma_subsampling:  <start>")
+        self.logger.info("_chroma_subsampling:  <start>")
     
         Y = ycbcr_pixels[:, :, 0]
         Cb = ycbcr_pixels[:, :, 1]
@@ -168,7 +185,7 @@ class JpegCompressor:
         # Обрезаем обратно до оригинальных размеров
         subsampled = subsampled_padded[:original_height, :original_width, :]
         
-        print("_chroma_subsampling:  <end>\n")
+        self.logger.info("_chroma_subsampling:  <end>")
         
         return subsampled
         
@@ -176,7 +193,7 @@ class JpegCompressor:
     def _split_into_blocks(self, subsampled):
         """Разбиение на блоки 8x8 с паддингом"""
         
-        print(f"\n_split_into_blocks: <start>")
+        self.logger.info(f"_split_into_blocks: <start>")
         
         Y = subsampled[:, :, 0]
         Cb = subsampled[:, :, 1]
@@ -217,7 +234,7 @@ class JpegCompressor:
         Cb_blocks = split_channel(Cb_padded)
         Cr_blocks = split_channel(Cr_padded)
         
-        print(f"_split_into_blocks: <end>\n")
+        self.logger.info(f"_split_into_blocks: <end>")
         
         return {
             'Y_blocks': Y_blocks,
@@ -248,15 +265,17 @@ class JpegCompressor:
         
     #     return dct_block
     
+    
     def _dct(self, block):
         """Применяет 2D DCT к одному блоку 8x8. Возвращает блок 8x8 DCT коэффициентов"""
+        block = block - 128
         return self.DCT_MATRIX @ block @ self.DCT_MATRIX.T
     
 
     def _apply_dct(self, blocks_data):
         """Применяет DCT ко всем блокам всех каналов"""
         
-        print(f"\n_apply_dct: <start>")
+        self.logger.info(f"_apply_dct: <start>")
         
         Y_blocks = blocks_data['Y_blocks']
         Cb_blocks = blocks_data['Cb_blocks']
@@ -276,7 +295,7 @@ class JpegCompressor:
                 Cb_dct[i, j] = self._dct(Cb_blocks[i, j])
                 Cr_dct[i, j] = self._dct(Cr_blocks[i, j])
         
-        print("_apply_dct: <end>\n")
+        self.logger.info("_apply_dct: <end>")
         
         return {
             'Y_dct': Y_dct,
@@ -288,7 +307,7 @@ class JpegCompressor:
     def _apply_quantization(self, dct_blocks):
         """Применяет квантование ко всем DCT-блокам всех каналов"""
 
-        print(f"\n_apply_quantization: <start>")
+        self.logger.info(f"_apply_quantization: <start>")
 
         Y_dct = dct_blocks['Y_dct']
         Cb_dct = dct_blocks['Cb_dct']
@@ -308,29 +327,52 @@ class JpegCompressor:
                 Cb_quant[i, j] = np.round(Cb_dct[i, j] / self._scale_quant_table(self.STANDARD_CHROMINANCE_QUANT_TABLE, self.quality).astype(np.float32)).astype(np.int32)
                 Cr_quant[i, j] = np.round(Cr_dct[i, j] / self._scale_quant_table(self.STANDARD_CHROMINANCE_QUANT_TABLE, self.quality).astype(np.float32)).astype(np.int32)
 
-        print("_apply_quantization: <end>\n")
+        self.logger.info("_apply_quantization: <end>")
 
         return {
             'Y_quant': Y_quant,
             'Cb_quant': Cb_quant,
             'Cr_quant': Cr_quant
         }
-    
-    
+
+
     def _dc_differentiation(self, quantized):
         """Дифференциальное кодирование DC-коэффициентов"""
+        
+        self.logger.info("_dc_differentiation: <start>")
+
+        def diff_dc(blocks):
+            # Извлекаем DC-компоненты всех блоков (заранее преобразовав блок 8х8 в одномерный массив с поомщью flatten())
+            dc = blocks[:, :, 0, 0].flatten()
+            # Дифференциальное кодирование (первый элемент берём как есть)
+            return np.diff(np.insert(dc, 0, 0))
+
+        self.logger.info("_dc_differentiation: <end>")
+        
+        return {
+            'Y_dc_diff': diff_dc(quantized['Y_quant']),
+            'Cb_dc_diff': diff_dc(quantized['Cb_quant']),
+            'Cr_dc_diff': diff_dc(quantized['Cr_quant']),
+        }
+
+
+    def _zigzag_scanning(self, block):
+        """Зигзаг-сканирование одного блока 8x8"""
         pass
 
-    def _zigzag_scanning(self, matrix):
-        """Зигзаг-сканирование"""
+
+    def _run_length_encoding(self, ac_coeffs):
+        """RLE кодирование AC-компонент"""
         pass
 
-    def _run_length_encoding(self, data):
-        """RLE кодирование"""
-        pass
 
-    def _huffman_encoding(self, data):
-        """Кодирование Хаффмана"""
+    def _huffman_encoding(self, rle_data):
+        """Хаффман-кодирование"""
+        pass
+    
+    
+    def _create_jpeg(self, encoded_data, output_path):
+        """Создание итогового JPEG файла из закодированных данных"""
         pass
     
     
@@ -339,7 +381,7 @@ class JpegCompressor:
     def load_image(self, image_path, quality=75):
         """Загрузка изображения (сбрасывает предыдущее состояние)"""
         
-        print("\nload_image:  <start>")
+        self.logger.info("load_image:  <start>")
         
         # Сбрасываем все предыдущие данные
         self._reset_state()
@@ -358,36 +400,85 @@ class JpegCompressor:
             
         except Exception as e:
             self._reset_state()
-            raise ValueError(f"Ошибка загрузки изображения: {e}")
+            raise ValueError(f"Image loading Error: {e}")
         
-        print("load_image:  <end>\n")
-        
-        
-    def _reset_state(self):
-        
-        print("_reset_state:  <start>")
-        
-        self._original_pixels = None
-        self._compressed_pixels = None
-        self.quality = None
-        
-        print("_reset_state:  <end>")
-        
-        
-    def _create_jpeg(self, encoded_data, output_path):
-        """Создание итогового JPEG файла из закодированных данных"""
-        pass
+        self.logger.info("load_image:  <end>")
 
 
     def compress(self, compressed_image_name: str):
         """Основной метод сжатия"""
         
         if self._original_pixels is None:
-            raise ValueError("Сначала загрузите изображение с помощью load_image()")
+            raise ValueError("Error: _original_pixels is None! First download image with \"load_image()\"")
             
         rgb_pixels = self._original_pixels
-        ycbcr_pixels = self._rgb_to_ycbcr(rgb_pixels)
-        subsampled = self._chroma_subsampling(ycbcr_pixels)
-        dict_blocks = self._split_into_blocks(subsampled)
-        dict_dct_blocks = self._apply_dct(dict_blocks)
-        dict_quant_blocks = self._apply_quantization(dict_dct_blocks)
+        try:
+            ycbcr_pixels = self._rgb_to_ycbcr(rgb_pixels)
+            self.logger.debug(f"""
+    Transform RGB → YCbCr: success
+    Size of ycbcr_pixels: {ycbcr_pixels.shape}
+    2x2 block:
+        {ycbcr_pixels[0][0]} {ycbcr_pixels[0][1]}
+        {ycbcr_pixels[1][0]} {ycbcr_pixels[1][1]}\n""")
+            
+        except Exception as e:
+            raise RuntimeError(f"Transforming RGB → YCbCr Error: {e}")
+        
+        try:
+            subsampled = self._chroma_subsampling(ycbcr_pixels)
+            self.logger.debug(f"""
+    YCbCr-array subsample: success
+    Size of subsampled_pixels: {subsampled.shape}
+    2x2 block:
+        {subsampled[0][0]} {subsampled[0][1]}
+        {subsampled[1][0]} {subsampled[1][1]}\n""")
+            
+        except Exception as e:
+            raise RuntimeError(f"YCbCr-array subsampling Error: {e}")
+        
+        try:
+            dict_blocks = self._split_into_blocks(subsampled)
+            block_str = "\n\n".join("   ".join(f"{val:.5f}" for val in row) for row in dict_blocks['Y_blocks'][0][0])
+            self.logger.debug(f"""
+    Split into block 8x8: success
+    Number of blocks: {dict_blocks['Y_blocks'].shape[0]}x{dict_blocks['Y_blocks'].shape[1]}
+    Size of Y: {dict_blocks['Y_blocks'].shape}
+    First Y block:\n
+{block_str}\n""")
+            
+        except Exception as e:
+            raise RuntimeError(f"Splittings into block 8x8 Error: {e}")
+        
+        try:
+            dict_dct_blocks = self._apply_dct(dict_blocks)
+            dct_block_str = "\n\n".join("   ".join(f"{val:.3f}" for val in row) for row in dict_dct_blocks['Y_dct'][0][0])
+            self.logger.debug(f"""
+    DCT-encode: success
+    Number of dct blocks: {dict_dct_blocks['Y_dct'].shape[0]}x{dict_dct_blocks['Y_dct'].shape[1]}
+    Size of dct-Y: {dict_dct_blocks['Y_dct'].shape}
+    DC-componet: {dict_dct_blocks['Y_dct'][0][0][0][0]}
+    First dct-Y block:\n
+{dct_block_str}\n""")
+            
+        except Exception as e:
+            raise RuntimeError(f"DCT-encoding Error: {e}")
+        
+        try:
+            dict_quant_blocks = self._apply_quantization(dict_dct_blocks)
+            quant_block_str = "\n\n".join("   ".join(f"{val:.3f}" for val in row) for row in dict_quant_blocks['Y_quant'][0][0])
+            self.logger.debug(f"""
+    Quantization: success
+    Number of quant blocks: {dict_quant_blocks['Y_quant'].shape[0]}x{dict_quant_blocks['Y_quant'].shape[1]}
+    Size of quant-Y: {dict_quant_blocks['Y_quant'].shape}
+    First quant-Y block:\n
+{quant_block_str}\n""")
+            
+        except Exception as e:
+            raise RuntimeError(f"Quantization Error: {e}")
+        
+        # try:
+        #     dict_dc_diff = self._dc_differentiation(dict_quant_blocks)
+        #     self.logger.debug(f"DC-differentiation: success")
+            
+        # except Exception as e:
+        #     raise RuntimeError(f"DC-differentiation Error: {e}")
--- main.py
+++ main.py
@@ -1,73 +1,4 @@
 from jpeg_compressor import JpegCompressor
-        
-def test1():
-    print("\n=== test1() ===")
-    
-    compressor = JpegCompressor()
-    
-    try:
-        compressor.load_image("data/test_image_2.jpg")
-        print("✅ Изображение загружено:")
-        print(f"   Размер: {compressor._original_pixels.shape}")
-        rgb = compressor._original_pixels.copy()
-        print(rgb[0][0], rgb[0][1])
-        print(rgb[1][0], rgb[1][1])
-
-    except Exception as e:
-        print(f"❌ Ошибка загрузки: {e}")
-        return
-    
-    try:
-        ycbcr_pixels = compressor._rgb_to_ycbcr(rgb)
-        print("✅ Преобразование RGB → YCbCr выполнено:")
-        print(f"   Размер: {ycbcr_pixels.shape}")
-        print(ycbcr_pixels[0][0], ycbcr_pixels[0][1])
-        print(ycbcr_pixels[1][0], ycbcr_pixels[1][1])
-        
-    except Exception as e:
-        print(f"❌ Ошибка преобразования: {e}")
-        
-    try:
-        subsampled = compressor._chroma_subsampling(ycbcr_pixels)
-        print("✅ Прореживание YCbCr-массива выполнено:")
-        print(f"   Размер: {subsampled.shape}")
-        print(subsampled[0][0], subsampled[0][1])
-        print(subsampled[1][0], subsampled[1][1])
-    except Exception as e:
-        print(f"❌ Ошибка прореживания: {e}")
-        
-        
-    try:
-        dict_blocks = compressor._split_into_blocks(subsampled)
-        print("✅ Разделение на блоки 8х8 выполнено:")
-        print(dict_blocks['Y_blocks'][0][0])
-        print(f"   Блоков: {dict_blocks['Y_blocks'].shape[0]}x{dict_blocks['Y_blocks'].shape[1]}")
-        print(f"   Размер: {dict_blocks['Y_blocks'].shape}")
-    except Exception as e:
-        print(f"❌ Ошибка разделения на блоки 8х8: {e}")
-        
-        
-    try:
-        dict_dct_blocks = compressor._apply_dct(dict_blocks)
-        print("✅ DCT-кодирование выполнено:")
-        print(dict_dct_blocks['Y_dct'][0][0])
-        print(f"   Обработано блоков: {dict_dct_blocks['Y_dct'].shape[0] * dict_dct_blocks['Y_dct'].shape[1]} на канал")
-        print(f"   Размер: {dict_dct_blocks['Y_dct'].shape}")
-        
-    except Exception as e:
-        print(f"❌ Ошибка DCT-кодирования: {e}")
-        
-        
-    try:
-        dict_quant_blocks = compressor._apply_quantization(dict_dct_blocks)
-        print("✅ Квантование блоков выполнено:")
-        print(dict_quant_blocks['Y_quant'][0][0])
-        print(f'   {compressor.quality=}')
-        print(f"   Размер: {dict_quant_blocks['Y_quant'].shape}")
-        
-    except Exception as e:
-        print(f"❌ Ошибка квантования: {e}")
-    
     
 def test_scale():
     compressor2 = JpegCompressor()
@@ -80,7 +11,11 @@ def test_scale():
     print("Отмасштабировання STANDARD_LUMINANCE_QUANT_TABLE при quality = 25")
     print(compressor2._scale_quant_table(compressor2.STANDARD_LUMINANCE_QUANT_TABLE, 25))
     
+    
+def test():
+    compressor = JpegCompressor()
+    compressor.load_image("data/test_image_2.jpg")
+    compressor.compress("output")
 
 if __name__ == "__main__":
-    #test_quant()
-    test1()
+    test()
