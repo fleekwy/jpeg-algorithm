38a0b05fd65749f8b106c1b7289012d6c5e902cb
Daniil Kondratyuk <daniilkondratuk@mail.com>
2025-10-13 00:43:04 +0300

    change main, save output compressed images in different qualities

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 from jpeg_compressor import JpegCompressor
-from PIL import Image, ImageFile
-import numpy as np
     
-def test_scale():
-    compressor2 = JpegCompressor()
-    print("Исходная STANDARD_LUMINANCE_QUANT_TABLE:")
-    print(compressor2.STANDARD_LUMINANCE_QUANT_TABLE)
-    print("Отмасштабировання STANDARD_LUMINANCE_QUANT_TABLE при quality = 75")
-    print(compressor2._scale_quant_table(compressor2.STANDARD_LUMINANCE_QUANT_TABLE, 75))
-    print("Отмасштабировання STANDARD_LUMINANCE_QUANT_TABLE при quality = 50")
-    print(compressor2._scale_quant_table(compressor2.STANDARD_LUMINANCE_QUANT_TABLE, 50))
-    print("Отмасштабировання STANDARD_LUMINANCE_QUANT_TABLE при quality = 25")
-    print(compressor2._scale_quant_table(compressor2.STANDARD_LUMINANCE_QUANT_TABLE, 25))
-    
-    
-def test():
+def main():
     compressor = JpegCompressor()
-    compressor.compress("data/test_image_3.jpg", "output_test_image_3")
-
-def test_rle():
-    compressor = JpegCompressor()
-    ac=[0, 8, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    res=compressor._run_length_encoding(ac)
-    print(res["rle"])
     
-def output_test():
-    ImageFile.LOAD_TRUNCATED_IMAGES = True
-    img = Image.open("data/output_test_image_3.jpg")
-    img.load()
+    compressor.compress("data/test_image.jpg", "compressed_image_q75", 75)
+    compressor.compress("data/test_image.jpg", "compressed_image_q20", 20)
+    compressor.compress("data/test_image.jpg", "compressed_image_q15", 15)
+    compressor.compress("data/test_image.jpg", "compressed_image_q10", 10)
+    compressor.compress("data/test_image.jpg", "compressed_image_q8", 8)
+    compressor.compress("data/test_image.jpg", "compressed_image_q6", 6)
+    compressor.compress("data/test_image.jpg", "compressed_image_q4", 4)
+    compressor.compress("data/test_image.jpg", "compressed_image_q1", 1)
 
 if __name__ == "__main__":
-    test()
+    main()
